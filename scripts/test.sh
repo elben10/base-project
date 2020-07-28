a7f7f68e5e29e6272d9028d12695f56989ab4ed3
Jakob Jul Elben <jakob@datamaga.com>
2020-07-28 08:49:43 +0200

    ...

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,9 +12,9 @@ docker-compose \
 -f docker-compose.yml \
 config > docker-stack.yml
 
-docker-compose -f docker-stack.yml build --parallel
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-stack.yml build --parallel
 docker-compose -f docker-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
 docker-compose -f docker-stack.yml up -d
 docker-compose -f docker-stack.yml exec -T backend bash /app/tests-start.sh "$@"
 docker-compose -f docker-stack.yml down -v --remove-orphans
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-stack.yml push
+docker-compose -f docker-stack.yml push
