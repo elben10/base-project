fe917830abfd190f1e8b80924ae70176022ee162
Jakob Jul Elben <jakob@datamaga.com>
2020-07-28 10:14:39 +0200

    ...

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,10 @@
 # Exit in case of error
 set -e
 
+docker-compose -f docker-compose.yml pull
+
 TAG=${TAG?Variable not set} \
 FRONTEND_ENV=${FRONTEND_ENV-production} \
 docker-compose \
 -f docker-compose.yml \
---pull \
 build
